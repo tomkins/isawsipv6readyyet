89f86cad7060a8be54d4e008e9270afba880679f
Alex Tomkins <tomkins@darkzone.net>
2023-08-01 22:08:40 +0100

    Watch JSON files for changes

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,13 @@
 module.exports = function(eleventyConfig) {
-    // Return your Object options:
+    eleventyConfig.addWatchTarget("./src/_data/*.json");
+
     return {
-      dir: {
-        input: "src",
-        output: "_site"
-      },
-      templateFormats: ["html", "njk", "md", "11ty.js"],
-      htmlTemplateEngine: "njk",
-      markdownTemplateEngine: "njk",
+        dir: {
+            input: "src",
+            output: "_site"
+        },
+        templateFormats: ["html", "njk", "md", "11ty.js"],
+        htmlTemplateEngine: "njk",
+        markdownTemplateEngine: "njk",
     }
-  };
+};
