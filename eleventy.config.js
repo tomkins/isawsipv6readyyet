69ffb0388b30be783fd45de884844c80c858b608
Alex Tomkins <tomkins@darkzone.net>
2023-08-02 23:42:43 +0100

    Use ELEVENTY_BASE_URL for base URL

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,10 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addWatchTarget("./src/_data/*.json");
+    eleventyConfig.addPlugin(EleventyHtmlBasePlugin, {
+        baseHref: process.env.ELEVENTY_BASE_URL || "/"
+    });
 
     return {
         dir: {
