c1d29cc6dc1995192f51b0da431c1d724335d362
Alex Tomkins <tomkins@darkzone.net>
2023-08-02 22:18:05 +0100

    Add a separate page for zero ratings

--- src/_data/aws_services.js
+++ src/_data/aws_services.js
@@ -1,8 +1,12 @@
 const all_services = require("./all_services.json");
-const rated_services = all_services.filter(service => service.score !== null);
+const all_rated_services = all_services.filter(service => service.score !== null);
+const rated_services = all_services.filter(service => service.score !== null && service.score > 0);
+const zero_score_services = all_services.filter(service => service.score === 0);
 const unrated_services = all_services.filter(service => service.score === null);
 
 module.exports = {
+    all_rated_services: all_rated_services,
     rated_services: rated_services,
+    zero_score_services: zero_score_services,
     unrated_services: unrated_services
 }
